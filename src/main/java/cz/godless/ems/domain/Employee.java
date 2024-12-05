9fe7325b6e8e5681adcffceb90d766c7be0b7d9a
1GodLess5 <gacekalbert@seznam.cz>
2024-12-05 20:33:08 +0100

    + Employee Domain

---
+++ src/main/java/cz/godless/ems/domain/Employee.java
@@ -0,0 +1,17 @@
+package cz.godless.ems.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
